ccbc2b2c41828f60be278c4bfd340e63ebd52a46
Fu-Qingchen <fuqingchen@whut.edu.cn>
2018-10-31 00:25:22 +0800

    新增：机械优化设计

---
+++ OptimalDesignOfMachine/Gedian/Gedian.c
@@ -0,0 +1,74 @@
+/*************************************************
+* @Author: Fu_Qingchen
+* @Date: 2018-10-30
+* @Description: 格点法求解迭代步长
+*************************************************/
+#include<stdio.h>
+#include<stdlib.h>
+#define N 9 //等分数-1
+
+/*************************************************
+* @Description: 要找极小点的函数
+*************************************************/
+double function(double x) {
+	return (x * x - x + 1);
+}
+
+/*************************************************
+* @Description: 平分区间[a,b]为(n+1)等分
+* @Input: a:区间左端点,b:区间右端点,A:等分点数组
+* @Return: A[i]
+*************************************************/
+void findA(double a, double b, double *A) {
+	for (int i = 0; i < N; i++)
+	{
+		*(A + i) = (b - a) / (N + 1)*(i + 1) + a;
+	}
+}
+
+/*************************************************
+* @Description: 求数组最小值序号
+* @Input: A:数组
+* @Return: 最小值序号
+*************************************************/
+int findMin(double *A) {
+	double tem = *(A + 0);
+	int num = 0;
+	for (int i = 0; i < N; i++)
+	{
+		if (tem > *(A+i))
+		{
+			num = i;
+			tem = *(A + i);
+		}
+	}
+	return num;
+}
+
+void main() {
+	double a = -80, b = 90, varepsilon = 0.005, result;
+	//a:区间左端点，b:区间右端点，varepsilon：精度，result：结果
+	int num;
+	double A[N],F[N];	//A为等分的点，F为等分点的函数值
+	do
+	{
+		findA(a, b, A);	//得到等分点
+		for (int i = 0; i < N; i++)	//得到等分点对应函数值
+		{
+			F[i] = function(A[i]);
+		}
+		num = findMin(F);
+		switch (num)	//迭代
+		{
+		case 0:a = a; b = A[num + 1]; break;
+		case N:b = b; a = A[num - 1]; break;
+		default:a = A[num - 1];b = A[num + 1];break;
+		}
+	} while (b - a > 0 ? ((b - a) > varepsilon) : ((a - b) > varepsilon));
+	result = 1.0 / 2 * (a + b);
+	printf("%d\n", num);
+	printf("%f\n", A[num]);
+	printf("%f\n", F[num]);
+	printf("Result:%f\n", result);
+	system("pause");
+}
