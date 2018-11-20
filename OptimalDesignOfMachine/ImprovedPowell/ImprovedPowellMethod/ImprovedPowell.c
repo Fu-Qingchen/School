b3c73b9c39486756a276ed9ffdd0922472a2c3e2
Fu-Qingchen <fuqingchen@whut.edu.cn>
2018-11-21 00:04:30 +0800

    新增：无约束优化

--- OptimalDesignOfMachine/ImprovedPowell/ImprovedPowellMethod/ImprovedPowell.c
+++ OptimalDesignOfMachine/ImprovedPowell/ImprovedPowellMethod/ImprovedPowell.c
@@ -1,22 +1,29 @@
 /*************************************************
 * @Author: Fu_Qingchen
-* @Date: 2018-10-30
+* @Date: 2018-10-20
 * @Description: 格点法求解迭代步长
 *************************************************/
 #include<stdio.h>
 #include<stdlib.h>
-#define A0 -9999	
-#define H 10	//格点法步长
-#define N 10 //格点法等分数
-#define V 0.00001	//精度
+#define	A0		100			//给定初值
+#define	V		0.000001	//给定精度
+#define N		2			//函数变量个数
+#define H		10			//格点法步长
+#define N_1d	10			//格点法等分数
 
 /*************************************************
-* @Description: 要找极小点的函数
+* @Description: 代求极小点函数
 *************************************************/
-double function(double x) {
-	return (x * x + 2 * x + 1);
+double function_main(double x1,double x2) {
+	return 10 * (x1 + x2 - 5)*(x1 + x2 - 5) + (x1 - x2)*(x1 - x2);
 }
 
+/*************************************************
+* @Description: 一维搜索要找极小点的函数
+*************************************************/
+double function_1d(double x) {
+	return (x * x - 2 * x + 1);
+}
 
 /*************************************************
 * @Description: 进退法主函数
@@ -24,18 +31,18 @@ double function(double x) {
 * @Return: 区间左右端点
 *************************************************/
 void jintui(double a0, double h, double *range) {
-	if (function(a0) == function(a0 + h))
+	if (function_1d(a0) == function_1d(a0 + h))
 	{
 		*(range) = a0;
 		*(range + 1) = a0 + h;
 	}
-	else if (function(a0) > function(a0 + h))
+	else if (function_1d(a0) > function_1d(a0 + h))
 	{
 		*(range) = a0;
 		do
 		{
 			a0 += h;
-		} while (function(a0)>function(a0 + h));
+		} while (function_1d(a0)>function_1d(a0 + h));
 		*(range + 1) = a0 + h;
 	}
 	else
@@ -44,11 +51,10 @@ void jintui(double a0, double h, double *range) {
 		do
 		{
 			a0 -= h;
-		} while (function(a0)<function(a0 + h));
+		} while (function_1d(a0)<function_1d(a0 + h));
 		*(range) = a0 - h;
 	}
 }
-
 
 /*************************************************
 * @Description: 平分区间[a,b]为(n+1)等分
@@ -56,9 +62,9 @@ void jintui(double a0, double h, double *range) {
 * @Return: A[i]
 *************************************************/
 void findA(double a, double b, double *A) {
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < N_1d; i++)
 	{
-		*(A + i) = (b - a) / (N + 1)*(i + 1) + a;
+		*(A + i) = (b - a) / (N_1d + 1)*(i + 1) + a;
 	}
 }
 
@@ -70,9 +76,9 @@ void findA(double a, double b, double *A) {
 int findMin(double *A) {
 	double tem = *(A + 0);
 	int num = 0;
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < N_1d; i++)
 	{
-		if (tem > *(A+i))
+		if (tem > *(A + i))
 		{
 			num = i;
 			tem = *(A + i);
@@ -88,20 +94,21 @@ int findMin(double *A) {
 *************************************************/
 double geDian(double a, double b, double varepsilon) {
 	int num;
-	double A[N], F[N];	//A为等分的点，F为等分点的函数值
+	double A[N_1d], F[N_1d];	//A为等分的点，F为等分点的函数值
 	do
 	{
 		findA(a, b, A);	//得到等分点
-		for (int i = 0; i < N; i++)	//得到等分点对应函数值
+		for (int i = 0; i < N_1d; i++)	//得到等分点对应函数值
 		{
-			F[i] = function(A[i]);
+			F[i] = function_1d(A[i]);
 		}
 		num = findMin(F);
 		switch (num)	//迭代
 		{
-		case 0:a = a; b = A[num + 1]; break;
-		case N:b = b; a = A[num - 1]; break;
-		default:a = A[num - 1]; b = A[num + 1]; break;
+		case 0:b = A[(num + 1)]; break;
+		case N_1d:a = A[(num - 1)]; break;
+		case N_1d - 1:a = A[(num - 1)]; break;
+		default:a = A[(num - 1)]; b = A[(num + 1)]; break;
 		}
 	} while (b - a > 0 ? ((b - a) > varepsilon) : ((a - b) > varepsilon));
 	return 1.0 / 2 * (a + b);
@@ -110,7 +117,7 @@ double geDian(double a, double b, double varepsilon) {
 void main() {
 	double a0 = A0, h = H, range[2] = { 0,0 }, varepsilon = V, result;
 	jintui(a0, h, range);
-	result = geDian(range[0],range[1],varepsilon);
-	printf("Result:%f\n", result);
+	result = geDian(range[0], range[1], varepsilon);
+	printf("Result:%lf\n", result);
 	system("pause");
 }
