d316c3447cf287f448013b6fa4ad182a146808ef
Fu-Qingchen <fuqingchen@whut.edu.cn>
2019-01-12 21:59:42 +0800

    结课更新

---
+++ HostComputer/LearnCsharp/Example.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YieldReturn语法解析
+{
+    class Program
+    {
+        Point a = new Point4D(1, 1, 1, 1);
+        Point4D b = new Point4D(1, 1, 1, 1);
+    }
+    public class Point
+    {
+        public int x, y;
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public class Point3D : Point
+    {
+        public int z;
+        public Point3D(int x, int y, int z) :
+            base(x, y)
+        {
+            this.z = z;
+        }
+    }
+    public class Point4D : Point3D
+    {
+        public int a;
+        public Point4D(int x, int y, int z,int a) :
+            base(x, y, z)
+        {
+            this.z = z;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,16 +1,3 @@
 # WorkSpace
 
 存放各种实验报告、大作业、课程设计、毕业设计的工程文件
-
-## 内容目录
-
-- `Examination` 期末复习相关
-
-- `FiniteElementMethod` 有限元分析
-- `MechanicalControlEngineering` 机械控制工程
-- `MechinaLearning` 机器学习
-- `Microcontroller` 单片机
-
-- `OptimalDesignOfMachine` 最优化技术
-- `StructuralMechanics` 结构力学
-
